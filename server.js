ece158447031bd7ccbdca64c9b9b87813084e83d
Leon Suarez <leonsua@gmail.com>
2022-04-06 20:36:38 -0400

    added helmet, initialize routes

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 require('dotenv').config({path: './config.env'});
 const express = require('express');
+const helmet = require('helmet');
 const connectDB = require('./config/db');
 
+
 const app = express();
+const auth = require('./routes/auth');
 connectDB;
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 3000;
 
