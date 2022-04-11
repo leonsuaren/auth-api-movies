8fdb3953aed4d6061ea562bd70964011abc99b14
Leon Suarez <leonsua@gmail.com>
2022-04-11 15:14:21 -0400

    connect to mongo atlas success

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+// mongosh "mongodb+srv://cluster0.wkvuo.mongodb.net/myFirstDatabase" --apiVersion 1 --username leonsuarez
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(`mongodb+srv://leonsuarez:${process.env.MONGO_ATLAS_PSW}@cluster0.wkvuo.mongodb.net/movies-auth?retryWrites=true&w=majority`, {
     useNewUrlParser: true
   });
   console.log('MongoDB connected');
